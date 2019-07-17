48c2f5d13b9049eb446cc6438f386746b3aa5db8
vivak <vivak.k@philips.com>
2019-07-17 17:51:24 +0530

    adding of redux with multiple action

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,15 +1,49 @@
 const initialstate = { counter: 0 }
 
 const reducer = (state = initialstate, action) => {
-    if (action.type == "ADDER")
-    {
-        return(
-            {...state,
-            counter: state.counter + 1}
-        )
-    }
+    // if (action.type == "ADDER")
+    // {
+    //     return(
+    //         {...state,
+    //         counter: state.counter + 1}
+    //     )
+    // }
+    
 
-    return state
+    // return state
+
+    switch (action.type){
+
+        case "ADDER":
+        {
+            return( {...state,
+                counter: state.counter + 1})
+        }
+
+        case "SUB":
+        {
+            return( {...state,
+                counter: state.counter - 1})
+        }
+
+        case "MULT":
+        {
+            return( {...state,
+                counter: state.counter * 2})
+        }
+
+        case "DIV":
+        {
+            return( {...state,
+                counter: state.counter / 2})
+        }
+
+        default:{
+            return(
+                state
+            )
+        }
+    }
 }
 
 export default reducer
